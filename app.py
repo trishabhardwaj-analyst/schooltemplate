41cc8df93bbfa6fe6f65c45d2aeb6ee8b305196e
trisha bhardwaj <trishabhardwaj@gmail.com>
2026-02-03 21:18:03 +0530

    fix about route

--- app.py
+++ app.py
@@ -2,13 +2,38 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/index')
 def index():
     return render_template('index.html')
+
 @app.route('/about')
 def about():
     return render_template('about.html')
 
+
+@app.route('/classes')
+def classes():
+    return render_template('classes.html')
+
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+
+@app.route('/team')
+def team():
+    return render_template('team.html')
+
+
+@app.route('/appointment')
+def appointment():
+    return render_template('appointment.html')
+
+
+@app.route('/call-to-action')
+def terms():
+    return render_template('call-to-action.html')
 
 
 if __name__ == '__main__':
